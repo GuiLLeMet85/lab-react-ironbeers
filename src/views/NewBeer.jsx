abd2e4eaaeb9f1107c62ae7ca2a6d0a41b51d7a2
Guillem B <guillembaracco@gmail.com>
2022-08-29 08:14:46 +0200

    iteration 5, error axios

--- src/views/NewBeer.jsx
+++ src/views/NewBeer.jsx
@@ -16,7 +16,7 @@ export default function NewBeer() {
         
     })
 
-    const [attenuationLevel, setAttenuationLevel] = useState('');
+    const [attenuationLevel, setAttenuationLevel] = useState(0);
 
     const handleChange= (e) => {
         setNewBeer(prev => {
@@ -34,16 +34,17 @@ export default function NewBeer() {
               attenuation_level: parseInt(e.target.value)
           }
       })
-  }
+    }
   
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-        const newBeer = await axios.post('https://ih-beers-api2.herokuapp.com/beers/new', newBeer);
-        navigate(`/new-beer/${newBeer.data.data._id}`)
-        } catch (error) {
-        console.error(error);
+            const newBeerData = await axios.post('https://ih-beers-api2.herokuapp.com/beers/new', newBeer);
+                navigate(`/new-beer/${newBeerData.data.data._id}`)
+        } 
+            catch (error) {
+                console.error(error);
         }
     }
 
@@ -51,17 +52,14 @@ export default function NewBeer() {
     <div>
          <h1>New Beer</h1>
         <form onSubmit={handleSubmit}>
-      
-        <input type="text" value={newBeer.name} onChange={handleChange} />
-        <input type="text" name="tagline" placeholder="Tagline" value={newBeer.tagline} onChange={handleChange} />
-        <input type="text" name="description" placeholder="Description" value={newBeer.description} onChange={handleChange} />
-        <input type="text" name="first_brewed" placeholder="First brewed" value={newBeer.first_brewed} onChange={handleChange} />
-        <input type="text" name="brewers_tips" placeholder="Brewers tips" value={newBeer.brewers_tips} onChange={handleChange} />
-        <input type="number" name="Attenuation_level" placeholder="Attenuation level" value={newBeer.attenuation_level} onChange={handleAttenLevel} />
-        <input type="text" name="contributed_by" placeholder="Contributed by" value={newBeer.contributed_by} onChange={handleChange} />
-
-
-        <button type="submit">Save</button>
+            <input type="text" name="name" placeholder="Title" value={newBeer.name} onChange={handleChange} />
+            <input type="text" name="tagline" placeholder="Tagline" value={newBeer.tagline} onChange={handleChange} />
+            <input type="text" name="description" placeholder="Description" value={newBeer.description} onChange={handleChange} />
+            <input type="text" name="first_brewed" placeholder="First brewed" value={newBeer.first_brewed} onChange={handleChange} />
+            <input type="text" name="brewers_tips" placeholder="Brewers tips" value={newBeer.brewers_tips} onChange={handleChange} />
+            <input type="number" name="Attenuation_level" placeholder="Attenuation level" value={newBeer.attenuation_level} onChange={handleAttenLevel} />
+            <input type="text" name="contributed_by" placeholder="Contributed by" value={newBeer.contributed_by} onChange={handleChange} />
+            <button type="submit">Save</button>
       </form>
     </div>
   )
